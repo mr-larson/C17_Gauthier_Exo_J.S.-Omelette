30aeca6579d8981442e3284e4db72f571d3ae30e
GAUTHIER <gauthierdewit@gmail.com>
2021-02-08 16:23:33 +0100

    étape en cours de l'omelette

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,28 +1,40 @@
 //EXO-Omelette
 //------------ les Class ---------------
 class Personne{
-    constructor(nom, lieu, argent, mainDroite=[], mainGauche=[]){
+    constructor(nom, lieu, argent){
         this.nom = nom
         this.lieu = lieu
         this.argent = argent
-        this.mainDroite = mainDroite
-        this.mainGauche = mainGauche
+        this.mainDroite = []
+        this.mainGauche = []
         this.seDeplacer = (depart, destination) => {
-            
+            depart.personnes.splice (depart.personnes.indexOf(this),1)
+            destination.personnes.push(this)
+            console.log (`${this.nom} va à  ${this.lieu.nom}`)
         }
-        this.payerAtricle = (articles) => {
-            
+        this.payerAtricle = (ingredients) => {
+            this.argent -= ingredients.prix
         }
         this.couper = (ingredients, outils) => {
-            
+            console.log(`${this.nom} prend ${this.mainDroite.nom} pour ${mainDroite.action}`);
+            for (let i=0; i<bol.contenu.length; i++){
+            if (ingredients[i].etat != "entier"){
+                console.log(`${ingredients[i].nom} est déjà coupé`);
+            }else{
+                ingredients[i].etat = "coupé"
+                console.log(`${ingredients[i].nom} a été coupé avec ${this.mainDroite.nom}`)
+            }
+        }
+        this.mainDroite = ""
         }
     }
 }
 
 class Lieu{
-    constructor(nom, personnes){
+    constructor(nom, personnes, panier){
         this.nom = nom
         this.personnes = personnes
+        this.panier = panier
     }
 }
 
@@ -45,7 +57,7 @@ let epicerie = new Lieu ("Epicerie", [], [
 ])
 
 //personnes
-let sabrina = new Personnes ("Sabrina", maison, 100)
+let sabrina = new Personnes ("Sabrina", maison.nom, 100, [], [])
 
 //outils
 let couteau = {
@@ -58,17 +70,23 @@ let poele = {
     nom = "Poele",
     contenu = [],
     cuire = () => {
-
+        setTimeout(() => {
+            this.contenu [0].etat = "cuit"
+            console.log (`${this.contenu[0].nom} est ${this.contenu[0].etat} !`)
+        }, 4000)
     }
 } 
 let bol = {
     nom = "bol",
     contenu = [],
-    melanger = () => {
-
+    melanger = (nomMelange) => {
+        let newMelange = {
+            nom: nomMelange,
+            etat: "cru"
+        }
+        this.contenu.push(newMelange)
     }
 }
-
 
 //ingredients
 let oeuf = new Ingredients ("Oeufs", "entier", 2)
@@ -78,4 +96,33 @@ let sel = new Ingredients ("Sel", "moulu", 1)
 let poivre = new Ingredients ("Poivre", "moulu", 1)
 let fromage = new Ingredients ("Fromage", "entier", 4)
 
+//------------- étape Omelette ------------------ 
+//liste course
 epicerie.ingredients = [oeuf, oignon, lait, sel, poivre, fromage]
+
+//Sabrina se déplace
+personne.push(maison)
+personne.seDeplacer(epicerie)
+
+//Sabrina récupère son panier de course
+personne.mainDroite = epicerie.panier[0];
+epicerie.panier.shift();
+console.log(`${personne.nom} a pris un ${personne.mainDroite.nom}`);
+console.log(`Il reste donc ${epicerie.panier.length} panier dans le stock des paniers de ${epicerie.nom}`)
+
+//Sabrina ajoute ses courses
+for (let i=0; i < epicerie.ingredients.length; i++){
+    personne.mainDroite.contenu.push(epicerie.ingredients[i])
+    console.log(`${personne.nom} a pris ${personne.mainDroite.contenu[i].nom}`)
+}
+
+//Sabrina paye les courses
+personne.payerArticle();
+console.log(`${personne.nom} a ${personne.argent} euros`)
+
+//Sabrina rentre à la maison
+personne.seDeplacer(maison)
+
+//Sabrina prépare à manger
+couteau.couper()
+bol.melanger()
